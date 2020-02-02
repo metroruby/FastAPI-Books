8afc7b3d2055cc354d6d6a4471de8ce93ee60708
Wisarut Leardsubmahasan <wisarut@opsta.co.th>
2020-02-02 19:29:16 +0700

    add outh2 for register api

--- main.py
+++ main.py
@@ -1,51 +1,146 @@
-from typing import List
+from datetime import datetime, timedelta
 
-from fastapi import FastAPI
+import jwt
+from fastapi import Depends, FastAPI, HTTPException
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jwt import PyJWTError
+from passlib.context import CryptContext
 from pydantic import BaseModel
+from starlette.status import HTTP_401_UNAUTHORIZED
 
-from flask import make_response, abort
+# to get a string like this run:
+# openssl rand -hex 32
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-class BookIn(BaseModel):
-    tname: str = None
-    author: str = None
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "disabled": False,
+    }
+}
+
+
+fake_book_db = {
+    "Book01": {
+        "bookname": "Book01",
+        "full_name": "John Doe",
+    }
+}
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str = None
+
+
+class User(BaseModel):
+    username: str
+    email: str = None
+    full_name: str = None
+    disabled: bool = None
+
+
+class Book(BaseModel):
+    bookname: str
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
 
 app = FastAPI()
 
-BOOKSTORE = {
-    "Measure What Matters": {
-        "tname": "Measure What Matters",
-        "author": "John Doerr",
-    },
-    "Misbehaving": {
-        "tname": "Misbehaving",
-        "author": "Richard H. Thaler",
-    },
-    "Pre-Suasion": {
-        "tname": "Pre-Suasion",
-        "author": "Robert Caildini",
-    },
-}
 
-@app.get("/books/")
-async def read_all():
-    return [BOOKSTORE[key] for key in sorted(BOOKSTORE.keys())]
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
 
-@app.get("/books/{tname}")
-async def read_one(tname):
-    if tname in BOOKSTORE:
-        titelbook = BOOKSTORE.get(tname)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(*, data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
     else:
-        abort(
-            404, "Book with last name {tname} not found".format(author=author)
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except PyJWTError:
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+@app.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
         )
-    return titelbook
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
 
-def fake_save_book(book_in: BookIn):
-    book_in_db = BookIn(**book_in.dict())
-    print("Book saved!")
-    return book_in_db
 
-@app.post("/books/", response_model=BookIn)
-async def create_book(*, book_in: BookIn):
-    book_saved = fake_save_book(book_in)
-    return book_saved
+@app.get("/book/", response_model=Book)
+async def read_book_all(current_user: Book = Depends(get_current_active_user)):
+    return current_user
